d3741f98542de0d4586e5f91b09a26954e97c94c
Rob Hess <robwhess@gmail.com>
2016-07-28 15:12:17 -0700

    Grab the git tag from Travis to set the version number (or use a default).

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 # Copyright 2016, Yahoo Inc.
 # Licensed under the terms of the Apache License, Version 2.0. See the LICENSE file associated with the project for terms.
 
+import os
+import packaging.version as version
+
 from setuptools import setup
 
 LONG_DESCRIPTION = """
@@ -12,9 +15,12 @@ those operations.  Such graphs are useful in computer vision, machine learning,
 and many other domains.
 """
 
+# Grab the version as the tag name from the Travis build, or else use a default.
+VERSION = os.environ.get('TRAVIS_TAG', '1.0')
+
 setup(
      name='graphkit',
-     version=1.0,
+     version=version.parse(VERSION).public,
      description='Lightweight computation graphs for Python',
      long_description=LONG_DESCRIPTION,
      author='Huy Nguyen, Arel Cordero, Pierre Garrigues, Rob Hess, Tobi Baumgartner, Clayton Mellina',
