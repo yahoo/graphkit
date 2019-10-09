510a9cd2d0e2a94e7f567523ca6b9d61914ceb76
Kostis Anagnostopoulos <ankostis@gmail.com>
2019-10-09 22:13:08 +0300

    chore(ver): bump version 1.2.4-->1.3.0

--- graphkit/__init__.py
+++ graphkit/__init__.py
@@ -2,7 +2,7 @@
 # Licensed under the terms of the Apache License, Version 2.0. See the LICENSE file associated with the project for terms.
 
 __author__ = 'hnguyen'
-__version__ = '1.2.4'
+__version__ = '1.3.0'
 
 from .functional import operation, compose
 
