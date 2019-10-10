cb3297ffb0a7352051f45865622dd5a3cbab26c0
Kostis Anagnostopoulos <ankostis@gmail.com>
2019-10-10 12:14:16 +0300

    doc: add project coords in package __strings__

--- graphkit/__init__.py
+++ graphkit/__init__.py
@@ -1,8 +1,13 @@
 # Copyright 2016, Yahoo Inc.
 # Licensed under the terms of the Apache License, Version 2.0. See the LICENSE file associated with the project for terms.
+"""Lightweight computation graphs for Python."""
 
 __author__ = "hnguyen"
 __version__ = "1.3.0"
+__license__ = "Apache-2.0"
+__title__ = "graphkit"
+__summary__ = __doc__.splitlines()[0]
+__uri__ = "https://github.com/yahoo/graphkit"
 
 from .functional import operation, compose
 from .modifiers import *  # noqa, on purpose to include any new modifiers
